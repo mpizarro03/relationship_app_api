bd2361964097f0cdf1eae106baa7a95ab2405652
Teddi Maull <teddimaull@gmail.com>
2018-05-02 10:40:49 -0600

    add created_at and remove user_id column, clean up code

--- models/received_scores.js
+++ models/received_scores.js
@@ -2,13 +2,13 @@ const knex = require('../knex')
 
 
 const getScores = (user_id,is_loved)=> {
-  return knex.select('user_id','score', 'feedback', 'description').from('scores')
+  return knex.select('score', 'feedback', 'description', 'created_at').from('scores')
   .where({to_user_id: user_id})
-  .join('users_feelings',"users_feelings.id", "scores.users_feelings_id")
-  .join('feelings',"users_feelings.feeling_id", "feelings.id")
+  .join('users_feelings',`users_feelings.id`, `scores.users_feelings_id`)
+  .join('feelings',`users_feelings.feeling_id`, `feelings.id`)
   .then( (scores)=> {
     if (!scores) {
-      return { status: 404, errors: `Could not find user scores` }
+      return { status: 404, errors: `Could not get scores that have been sent to this user` }
     }
 
     return scores
